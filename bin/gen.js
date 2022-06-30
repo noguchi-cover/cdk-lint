b5f567208876b2e8e3bae1be70ac11e3731aab4b
noguchi-cover <noguchi.takuma@cover-corp.com>
2022-06-30 18:13:42 +0900

    feat: add commands

--- bin/gen.js
+++ bin/gen.js
@@ -1,2 +1,33 @@
 #!/usr/bin/env node
-console.log('hello, npx');
+
+const path = require('path');
+const { execSync } = require('child_process');
+
+const fs = require('fs');
+const findFile = (startPath, filter) => {
+  var files = fs.readdirSync(startPath);
+  for (var i = 0; i < files.length; i++) {
+    var filename = path.join(startPath, files[i]);
+    if (filename == filter) {
+      return true;
+    }
+  }
+};
+
+if (!findFile('./', 'package.json')) {
+  console.log('could not find package.json, failed to setup');
+  return;
+}
+
+const isYarn = findFile('./', 'yarn.lock');
+const pm = isYarn ? 'yarn' : 'npm'; //package manager
+console.log(`set up lint tools with ${pm}...`);
+
+const installCommand = isYarn ? 'yarn add --dev' : 'npm i --save-dev';
+console.log('install eslint');
+execSync(
+  `${installCommand} eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin`
+);
+
+console.log('install prettier');
+execSync(`${installCommand} prettier`);
